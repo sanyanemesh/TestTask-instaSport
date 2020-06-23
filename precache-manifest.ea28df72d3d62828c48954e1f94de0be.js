5284e5f63cc08e20fac8708885f5787aebbb39e0
sanyanemesh <sanya.nemesh@gmail.comgit config --global user.name sanyanemeshgit config --global user.email sanya.nemesh@gmail.com>
2020-06-23 16:26:44 +0300

    Updates

---
+++ precache-manifest.ea28df72d3d62828c48954e1f94de0be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ae0be4882cf65253f9cb439803e608c",
+    "url": "/TestTask-instaSport/index.html"
+  },
+  {
+    "revision": "7fbc2e00c239577de541",
+    "url": "/TestTask-instaSport/static/css/main.0620377e.chunk.css"
+  },
+  {
+    "revision": "93d06f402db58b8e214e",
+    "url": "/TestTask-instaSport/static/js/2.3cea0474.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/TestTask-instaSport/static/js/2.3cea0474.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7fbc2e00c239577de541",
+    "url": "/TestTask-instaSport/static/js/main.24f3c39f.chunk.js"
+  },
+  {
+    "revision": "d70b0fb78bba967a7f5e",
+    "url": "/TestTask-instaSport/static/js/runtime-main.d2ea0994.js"
+  }
+]);
